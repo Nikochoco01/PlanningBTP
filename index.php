45e43f31ee5a9a4f19adb809bd3b38fdd3017f88
Gwendal FLUTET <gwendal.flutet@gmail.com>
2022-10-28 09:35:50 +0200

    include head.php remplacer par le code en lui-même

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <!DOCTYPE html>
 <html lang="fr">
-<?php $titre = "Accueil";
-include_once "module/head.php";?>
+<?php $titre = "Accueil"; ?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/CSS/default.css">
+    <link rel="stylesheet" href="/CSS/menu.css">
+    <link rel="stylesheet" href="/Icon/style.css">
+    <title> <?php echo $titre ?> </title>
+</head>
 
 <body>
     <?php include_once "module/header.php"; ?>
