7be7a58083bb4f679ae9dfd2352148fb4cfdfd78
Gwendal FLUTET <gwendal.flutet@gmail.com>
2023-01-03 08:43:26 +0100

    vehicle.js réparer

--- js/vehicle.js
+++ js/vehicle.js
@@ -4,12 +4,11 @@ const seatNbs = document.querySelectorAll("#maxPassenger option");
 function hideSeatBeyonMax(license, seatNbs){
     console.log(license.options[license.selectedIndex], license.options[license.selectedIndex].getAttribute("max"));
     for(let seatNb of seatNbs){
-        if(seatNb.value <= license.options[license.selectedIndex].getAttribute("max")){
+        if(parseInt(seatNb.value, 10) <= parseInt(license.options[license.selectedIndex].getAttribute("max"), 10)){
             seatNb.removeAttribute("hidden");
         }else{
             seatNb.setAttribute("hidden", '');
         }
-        console.log(seatNb, seatNb.value, seatNb.value <= license.options[license.selectedIndex].getAttribute("max"));
     }
 }
 hideSeatBeyonMax(license, seatNbs);
