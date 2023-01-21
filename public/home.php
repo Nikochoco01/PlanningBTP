68f0e6753c2408a4172a33621f27f8061f0223a5
Nikochoco01 <62524439+Nikochoco01@users.noreply.github.com>
2023-01-21 13:32:23 +0100

    mise en page du Layout principal

--- public/home.php
+++ public/home.php
@@ -15,26 +15,11 @@ include_once dirname(__FILE__,2)."/private/constant/page/head.php";
 ?>
 
 <body>
-    <?php include_once dirname(__FILE__,2). "/private/constant/page/header.php" ?>
-    <div class="layout"> 
+    <div class="layout">
+        <?php include_once dirname(__FILE__,2). "/private/constant/page/header.php" ?>
         <?php include_once dirname(__FILE__,2). "/private/constant/page/aside.php" ?>
             <main>
-                <section class="homePage">
-                    <h2> Bienvenue </h2>
-                    <!-- <img src="/private/img/defaultPP.png" alt="image de votre profil"> -->
-                    <p>
-                        <?= InputSecurity::displayWithFormat($_SESSION['userFirstName'] , "FirstName") ." ". 
-                            InputSecurity::displayWithFormat($_SESSION['userLastName'] , "LastName") 
-                        ?>
-                    </p>
-                    <p>
-                        <?= InputSecurity::displayWithFormat($_SESSION['userPosition'] , 'Position') ?>
-                    </p>
-                    <div class="scheduleSummary">
-                        <p><?= date('d') . '/' . date('m') .'/'. date('Y') ?> </p>
-                        <p> Horaires : <?= $_SESSION['schedule'] ?> </p>
-                    </div>
-                </section>
+                
             </main>
     </div>
 </body>
