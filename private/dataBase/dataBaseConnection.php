d598041fb55fcfbb9baf38a5c83ea7f8bcf27394
Gwendal FLUTET <gwendal.flutet@gmail.com>
2023-02-02 11:15:56 +0100

    up to date branch

--- private/dataBase/dataBaseConnection.php
+++ private/dataBase/dataBaseConnection.php
@@ -2,14 +2,14 @@
 /**
  * Gwendal
  */
-// try{
-//     $PDO = new PDO('mysql:host=iutbg-lamp.univ-lyon1.fr:3306;dbname=p2103916' , 'p2103916' , '12103916');
-//     $PDO->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
-//     $PDO->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE , PDO::FETCH_OBJ);
-// }
-// catch(PDOException $e){
-//     echo "Error : " . $e->getMessage();
-// }
+try{
+    $PDO = new PDO('mysql:host=iutbg-lamp.univ-lyon1.fr:3306;dbname=p2103916' , 'p2103916' , '12103916');
+    $PDO->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
+    $PDO->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE , PDO::FETCH_OBJ);
+}
+catch(PDOException $e){
+    echo "Error : " . $e->getMessage();
+}
 
 /**
  * PC Bruno
@@ -26,12 +26,11 @@
 /**
  * PC Nikola
  */
-try{
-    $PDO = new PDO('mysql:host=localhost:3306;dbname=bdsite' , 'root' , 'root');
-    $PDO->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
-    $PDO->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE , PDO::FETCH_OBJ);
-}
-catch(PDOException $e){
-    echo "Error : " . $e->getMessage();
-}
-
+// try{
+//     $PDO = new PDO('mysql:host=localhost:3306;dbname=bdsite' , 'root' , 'root');
+//     $PDO->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
+//     $PDO->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE , PDO::FETCH_OBJ);
+// }
+// catch(PDOException $e){
+//     echo "Error : " . $e->getMessage();
+// }
