e6fe8eb0aacabc8563cbd003b93ce237a21093d4
Nikochoco01 <62524439+Nikochoco01@users.noreply.github.com>
2023-03-17 10:11:34 +0100

    mise a jour des classes pour le CSS

--- private/profil/profilView.php
+++ private/profil/profilView.php
@@ -1,41 +1,44 @@
-<div class="profilView">
-    <img src="/private/treatment/indexProcess/export.php?pictureId=<?= $_SESSION['userId']; ?>" alt="photo de profil">
-    <p>
+<div class="profil-view-container">
+    <img class="profil-view-picture" src="/private/treatment/indexProcess/export.php?pictureId=<?= $_SESSION['userId']; ?>" alt="photo de profil">
+    <div class="profil-view-user-name">
         <span class="label"> <i class="icon-user"></i> Nom d'utilisateur:</span>
         <span class="userInfo">
             <?= $_SESSION['userName'] ?>
         </span>
-    </p>
-    <p>
+    </div>
+    <div class="profil-view-firstname">
         <span class="label"> Prénom:</span>
         <span class="userInfo"> 
             <?= InputSecurity::displayWithFormat($_SESSION['userFirstName'] , "uppercaseFirstLetter") ?>
         </span>
-    </p>
-    <p>
+    </div>
+    <div class="profil-view-lastname">
         <span class="label"> Nom:</span>
         <span class="userInfo">
             <?= InputSecurity::displayWithFormat($_SESSION['userLastName'] , "uppercase") ?>
         </span>
-    </p>
-    <p>
+    </div>
+    <div class="profil-view-fonction">
         <span class="label"> <i class="icon-briefcase"></i> Fonction:</span>
         <span class="userInfo">
             <?= InputSecurity::displayWithFormat($_SESSION['userPosition'] , "uppercase") ?>
         </span>
-    </p>
-    <p>
+    </div>
+    <div class="profil-view-mail-address">
         <span class="label"> <i class="icon-at"></i> Adresse mail:</span>
         <span class="userInfo">
             <?= $_SESSION['userMail'] ?>
         </span>
-    </p>
-    <p>
+    </div>
+    <div class="profil-view-mail-phone-number">
         <span class="label"> <i class="icon-phone"></i> Numéro de téléphone:</span>
         <span class="userInfo">
             <?= InputSecurity::displayWithFormat($_SESSION['userPhone'] , "PhoneNumber") ?>
         </span>
-    </p>
+    </div>
 
-    <a href="<?= URLManagement::addUrlParam(array('display' => PARAM_MODIFY_DISPLAY)) ?>"> <i class="icon-user-edit"></i> Modifier </a>
+    <div class="profil-view-btn-zone">
+        <a class="btn-link width-50 height-50 border-rad-10 bg-color-gray text-color-white" href="<?= URLManagement::addUrlParam(array('display' => PARAM_MODIFY_DISPLAY)) ?>"> <i class="icon-user-edit"></i> Modifier </a>
+    </div>
+    
 </div>
--- private/selectTab.php
+++ private/selectTab.php
@@ -4,13 +4,13 @@ if($path === '/profil'):
 <nav class="nav-tab">
     <ul class="tab-list">
         <li class="tab-list-element bg-color-gray <?= URLManagement::activeTab(PARAM_PERSONAL_ONGLET) ?>">
-            <a class="btn-link text-color-white" href=" <?= URLManagement::addUrlParam(array('onglet' => PARAM_PERSONAL_ONGLET , 'display' => PARAM_VIEW_DISPLAY)) ?>">
+            <a class="btn-link text-color-white width-100 height-100" href=" <?= URLManagement::addUrlParam(array('onglet' => PARAM_PERSONAL_ONGLET , 'display' => PARAM_VIEW_DISPLAY)) ?>">
                 <i class="icon-user margin-right-5"></i> Personnel
             </a>
         </li>
 
         <li class="tab-list-element bg-color-gray <?= URLManagement::activeTab(PARAM_EMPLOYEE_ONGLET) ?>">
-            <a class="btn-link text-color-white" href=" <?= URLManagement::addUrlParam(array('onglet' => PARAM_EMPLOYEE_ONGLET , 'display' => PARAM_VIEW_DISPLAY))?>">
+            <a class="btn-link text-color-white width-100 height-100" href=" <?= URLManagement::addUrlParam(array('onglet' => PARAM_EMPLOYEE_ONGLET , 'display' => PARAM_VIEW_DISPLAY))?>">
                <i class="icon-users-group margin-right-5"></i> Employés
             </a>
         </li>
@@ -25,26 +25,26 @@ if($path === '/profil'):
 
 <nav class="nav-tab">
     <ul class="tab-list">
-        <li class="buttonTabs <?= URLManagement::activeTab(PARAM_MISSION_ONGLET) ?>"> 
-            <a href=" <?= URLManagement::addUrlParam(array('onglet'=> PARAM_MISSION_ONGLET)) ?>">
+        <li class="tab-list-element bg-color-gray <?= URLManagement::activeTab(PARAM_MISSION_ONGLET) ?>"> 
+            <a class="btn-link text-color-white width-100 height-100" href=" <?= URLManagement::addUrlParam(array('onglet'=> PARAM_MISSION_ONGLET)) ?>">
                 <i class="icon-clipboard-list"></i> Missions 
             </a>
         </li>
         
-        <li class="buttonTabs <?= URLManagement::activeTab(PARAM_EMPLOYEE_ONGLET) ?>"> 
-            <a href=" <?= URLManagement::addUrlParam(array('onglet'=> PARAM_EMPLOYEE_ONGLET)) ?>">
+        <li class="tab-list-element bg-color-gray <?= URLManagement::activeTab(PARAM_EMPLOYEE_ONGLET) ?>"> 
+            <a class="btn-link text-color-white width-100 height-100" href=" <?= URLManagement::addUrlParam(array('onglet'=> PARAM_EMPLOYEE_ONGLET)) ?>">
                 <i class="icon-users-group"></i> Employés 
             </a>
         </li>
 
-        <li class="buttonTabs <?= URLManagement::activeTab(PARAM_VEHICLES_ONGLET) ?>"> 
-            <a href=" <?= URLManagement::addUrlParam(array('onglet'=> PARAM_VEHICLES_ONGLET)) ?>">
+        <li class="tab-list-element bg-color-gray <?= URLManagement::activeTab(PARAM_VEHICLES_ONGLET) ?>"> 
+            <a class="btn-link text-color-white width-100 height-100" href=" <?= URLManagement::addUrlParam(array('onglet'=> PARAM_VEHICLES_ONGLET)) ?>">
                 <i class="icon-warehouse"></i> Véhicules 
             </a>
         </li>
         
-        <li class="buttonTabs <?= URLManagement::activeTab(PARAM_MATERIAL_ONGLET) ?>"> 
-            <a href=" <?= URLManagement::addUrlParam(array('onglet'=> PARAM_MATERIAL_ONGLET)) ?>">
+        <li class="tab-list-element bg-color-gray <?= URLManagement::activeTab(PARAM_MATERIAL_ONGLET) ?>"> 
+            <a class="btn-link text-color-white width-100 height-100" href=" <?= URLManagement::addUrlParam(array('onglet'=> PARAM_MATERIAL_ONGLET)) ?>">
                 <i class="icon-tool"></i> Matériel 
             </a>
         </li>
