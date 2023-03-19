d92a77d28a67aec65a26a2525fc3dba675b0d297
Nikochoco01 <62524439+Nikochoco01@users.noreply.github.com>
2023-03-19 17:15:24 +0100

    modification classes profil View et profil modify pour simplifier le css

--- private/profil/profilModify.php
+++ private/profil/profilModify.php
@@ -1,7 +1,7 @@
-<form action="modifyEmployee" method="post" class="profil-modify-container" enctype="multipart/form-data">
+<form action="modifyEmployee" method="post" class="profil-container" enctype="multipart/form-data">
     <a href="<?= returnURL()?>" class="btn-link bg-color-orange border-rad-10 width-50px height-50px margin-top-10 margin-left-10" > <i class=""></i> X </a>
 
-    <div class="profil-view-picture">
+    <div class="profil-picture">
         <div class="input-picture-container">
         <input type="file" name="" id="input-picture-field" class="input-picture-field">
         <label for="input-picture-field" class="input-picture-label"> <i class="icon-image-plus"></i> </label>
@@ -10,44 +10,44 @@
 
     </div>
 
-    <div class="profil-view-user-name disabled input-container width-70">
+    <div class="profil-user-name disabled input-container width-70">
         <input type="text" name="userName" id="userName" value="<?= $_SESSION['userName'] ?>" class="disabled input-field bg-color-light-gray" disabled>
         <label for="userName" class="input-label font-bold text-color-white"> <i class="icon-user"></i> Nom d'utilisateur</label>
     </div>
 
-    <div class="profil-modify-password input-container width-70">
+    <div class="profil-password input-container width-70">
         <input type="password" name="userPassword" id="userPassword" value="" placeholder="taper un nouveau mot de passe" class="input-field bg-color-light-gray" optional>
         <label for="userPassword" class="input-label font-bold text-color-white"> <i class="icon-lock"></i> Mot de passe</label>
     </div>
 
-    <div class="profil-view-firstname input-container width-70">
+    <div class="profil-firstname input-container width-70">
         <input type="text" name="userFirstName" id="userFirstName" class="input-field bg-color-light-gray" value="<?= $_SESSION['userFirstName'] ?>" required>
         <label for="userFirstName" class="input-label font-bold text-color-white">Prénom</label>
     </div>
 
-    <div class="profil-view-lastname input-container width-70">
+    <div class="profil-lastname input-container width-70">
         <input type="text" name="userLastName" id="userLastName" class="input-field bg-color-light-gray" value="<?= $_SESSION['userLastName'] ?>" required>
         <label for="userLastName" class="input-label font-bold text-color-white">Nom</label>
     </div>
 
-    <div class="profil-view-fonction <?= addDisabled() ?> input-container width-70">
+    <div class="profil-fonction <?= addDisabled() ?> input-container width-70">
         <input type="text" name="userPosition" id="userPosition" class="input-field bg-color-light-gray <?= addDisabled() ?>" value="<?= $_SESSION['userPosition'] ?>" <?= disableInput()?> optional>
         <label for="userPosition" class="input-label font-bold text-color-white"> <i class="icon-briefcase"></i> Fonction</label>
     </div>
 
-    <div class="profil-view-mail-address input-container width-70">
+    <div class="profil-mail-address input-container width-70">
         <input type="text" name="userMail" id="userMail" class="input-field bg-color-light-gray" value="<?= $_SESSION['userMail'] ?>" required>
         <label for="userMail" class="input-label font-bold text-color-white"> <i class="icon-at"></i> Adresse mail</label>
     </div>
 
-    <div class="profil-view-phone-number input-container width-70">
+    <div class="profil-phone-number input-container width-70">
         <input type="text" name="userPhone" id="userPhone" class="input-field bg-color-light-gray" value="<?= $_SESSION['userPhone'] ?>" required>
         <label for="userPhone" class="input-label font-bold text-color-white"> <i class="icon-phone"></i> Numéro de téléphone</label>
     </div>
 
     <input type="hidden" name="userId" value="<?= $_SESSION['userId'] ?>">
 
-    <div class="profil-modify-btn-zone">
+    <div class="profil-btn-zone">
         <label for="btn-register" class="label-btn-input bg-color-orange text-color-black"> <span> Enregistrer </span> </label>
         <input type="submit" class="btn-input" id="btn-register">
 
--- private/profil/profilView.php
+++ private/profil/profilView.php
@@ -1,45 +1,41 @@
-<div class="profil-view-container">
-    <div class="profil-view-picture">
-        <img class="profil-picture" src="/img/defaultPP.png" alt="photo de profil">
-    </div>
-    <div class="profil-view-user-name">
-        <span class="label"> <i class="icon-user"></i> Nom d'utilisateur</span>
-        <span class="user-info">
-            <?= $_SESSION['userName'] ?>
-        </span>
-    </div>
-    <div class="profil-view-firstname">
-        <span class="label"> Prénom</span>
-        <span class="user-info"> 
-            <?= InputSecurity::displayWithFormat($_SESSION['userFirstName'] , "uppercaseFirstLetter") ?>
-        </span>
-    </div>
-    <div class="profil-view-lastname">
-        <span class="label"> Nom</span>
-        <span class="user-info">
-            <?= InputSecurity::displayWithFormat($_SESSION['userLastName'] , "uppercase") ?>
-        </span>
-    </div>
-    <div class="profil-view-fonction">
-        <span class="label"> <i class="icon-briefcase"></i> Fonction</span>
-        <span class="user-info">
-            <?= InputSecurity::displayWithFormat($_SESSION['userPosition'] , "uppercase") ?>
-        </span>
-    </div>
-    <div class="profil-view-mail-address">
-        <span class="label"> <i class="icon-at"></i> Adresse mail</span>
-        <span class="user-info">
-            <?= $_SESSION['userMail'] ?>
-        </span>
-    </div>
-    <div class="profil-view-phone-number">
-        <span class="label"> <i class="icon-phone"></i> Numéro de téléphone</span>
-        <span class="user-info">
-            <?= InputSecurity::displayWithFormat($_SESSION['userPhone'] , "PhoneNumber") ?>
-        </span>
+<div class="profil-container">
+    <div class="profil-picture">
+        <div class="input-picture-container">
+            <img src="/img/defaultPP.png" alt="photo de profil" class="input-picture-rendered">
+        </div>
     </div>
 
-    <div class="profil-view-btn-zone">
+    <div class="profil-user-name input-container width-70">
+        <input type="text" name="userName" id="userName" value="<?= $_SESSION['userName'] ?>" class="input-field bg-color-light-gray" readonly>
+        <label for="userName" class="input-label font-bold text-color-white"> <i class="icon-user"></i> Nom d'utilisateur</label>
+    </div>
+
+    <div class="profil-firstname input-container width-70">
+        <input type="text" name="userFirstName" id="userFirstName" class="input-field bg-color-light-gray" value="<?= InputSecurity::displayWithFormat($_SESSION['userFirstName'] , "uppercaseFirstLetter") ?>" readonly>
+        <label for="userFirstName" class="input-label font-bold text-color-white">Prénom</label>
+    </div>
+
+    <div class="profil-lastname input-container width-70">
+        <input type="text" name="userLastName" id="userLastName" class="input-field bg-color-light-gray" value="<?= InputSecurity::displayWithFormat($_SESSION['userLastName'] , "uppercase") ?>" readonly>
+        <label for="userLastName" class="input-label font-bold text-color-white">Nom</label>
+    </div>
+
+    <div class="profil-fonction input-container width-70">
+        <input type="text" name="userPosition" id="userPosition" class="input-field bg-color-light-gray" value="<?= InputSecurity::displayWithFormat($_SESSION['userPosition'] , "uppercase") ?>" readonly>
+        <label for="userPosition" class="input-label font-bold text-color-white"> <i class="icon-briefcase"></i> Fonction</label>
+    </div>
+
+    <div class="profil-mail-address input-container width-70">
+        <input type="text" name="userMail" id="userMail" class="input-field bg-color-light-gray" value="<?= $_SESSION['userMail'] ?>" readonly>
+        <label for="userMail" class="input-label font-bold text-color-white"> <i class="icon-at"></i> Adresse mail</label>
+    </div>
+
+    <div class="profil-phone-number input-container width-70">
+        <input type="text" name="userPhone" id="userPhone" class="input-field bg-color-light-gray" value="<?= InputSecurity::displayWithFormat($_SESSION['userPhone'] , "PhoneNumber") ?>" readonly>
+        <label for="userPhone" class="input-label font-bold text-color-white"> <i class="icon-phone"></i> Numéro de téléphone</label>
+    </div>
+
+    <div class="profil-btn-zone">
         <a class="btn-link width-50 height-50 border-rad-10 bg-color-orange text-color-white" href="<?= URLManagement::addUrlParam(array('display' => PARAM_MODIFY_DISPLAY)) ?>"> <i class="icon-user-edit"></i> Modifier </a>
     </div>
     
